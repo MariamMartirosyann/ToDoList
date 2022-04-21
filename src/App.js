3d98e4a3a30f8d401a9ab237aeecb000517f2fd3
Mariam Martirosyan <76228722+MariamMartirosyann@users.noreply.github.com>
2022-04-21 08:22:25 +0400

    current-update
    current-update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import  Typography  from '@mui/material/Typography';
 import Input from './Components/Input';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <Typography component={"h1"} variant={"h3"} className="mainTitle">ToDo List </Typography>
+    <div className="mainTitle">
+     <Typography component={"h1"} variant={"h3"} >ToDo List </Typography>
    <Input/>
     </div>
   );
--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import FormControl from "@mui/material/FormControl";
 import OutlinedInput from "@mui/material/OutlinedInput";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 import '../App.css';
 
 
 const Input = () => {
   return (
     <div className={"inputArea"}>
-      <FormControl sx={{ width: "25ch" }} >
+      <FormControl sx={{ width: "550px" }} >
         <OutlinedInput placeholder="Please enter text" />
+       < AddCircleIcon className="addIcon"/>
 
       </FormControl>
     </div>
